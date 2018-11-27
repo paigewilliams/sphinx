cabd254f4005b358e7e92d9875790c26260e8ea3
Paige Williams and Quinn Miller <paige@>
2018-11-27 10:36:40 -0800

    Add method to find answer to random riddle in browser.

--- app.rb
+++ app.rb
@@ -6,20 +6,20 @@ require('./lib/project')
 require('capybara')
 
 get('/')do
-  @riddle = random_riddle
-  @riddle.random_riddle()
+  @new_riddle = Riddles.new()
+  @answer = @new_riddle.find_answer
   binding.pry
   erb(:input)
 end
 
-post('/output') do
-
-  @answer = params.fetch("riddle")
-  @answer1 = params.fetch("riddle1")
-  @answer2 = params.fetch("riddle2")
-  # if @answer === "human" && @answer1 === "chessmen" && @answer2 === "teabag"
-  #   erb(:success)
-  # else
-  #   erb(:failure)
-  # end
-end
+# post('/output') do
+#
+#   @answer = params.fetch("riddle")
+#   @answer1 = params.fetch("riddle1")
+#   @answer2 = params.fetch("riddle2")
+#   # if @answer === "human" && @answer1 === "chessmen" && @answer2 === "teabag"
+#   #   erb(:success)
+#   # else
+#   #   erb(:failure)
+#   # end
+# end
--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,12 @@
 
 class Riddles
 
-  def generate_riddle
+  def initialize
     @riddle_hash = Hash.new()
-    @riddle_hash.store("What goes on fours in the morning, on twos in the
-    afternoon and on threes at night?", "human")
+    @riddle_hash.store("What goes on fours in the morning, on twos in the afternoon and on threes at night?", "human")
     @riddle_hash.store("What kind of men are always above board?", "chessmen")
     @riddle_hash.store("I start off dry but come out wet. I go in light and come out heavy. What am I?", "teabag")
+    random_riddle
   end
 
   def random_riddle
